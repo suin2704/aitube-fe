5df0d19640f168fb3336b1ef00ffcb3fea00f655
suin2704 <wlsuiny@gmail.com>
2026-04-27 11:53:17 +0900

    fix: service intro button - white bg + blue text, same as browse button

--- components/home/HeroBanner.tsx
+++ components/home/HeroBanner.tsx
@@ -33,7 +33,7 @@ export default function HeroBanner() {
             </Link>
             <Link href="/about">
               <Button
-                className="bg-white/20 border-2 border-white text-white hover:bg-white hover:text-blue-700 h-12 px-8 text-base font-semibold"
+                className="bg-white text-blue-700 hover:bg-blue-50 h-12 px-8 text-base font-semibold"
               >
                 서비스 소개
               </Button>
